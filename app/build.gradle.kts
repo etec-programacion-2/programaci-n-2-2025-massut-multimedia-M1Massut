98e4bee57da15b5fed5e93e238bdeccd88345a42
M1Massut <maximo.aznar.massut@gmail.com>
2025-09-23 13:03:20 -0300

    Falta terminar la coneccion entre Newrescet y la base de datos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 
     // This dependency is used by the application.
     implementation(libs.guava)
+    implementation("org.xerial:sqlite-jdbc:3.45.3.0")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
